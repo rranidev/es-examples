30206f1357a4c9e1fb38c53272d99eeeb1664e1e
surendar raparla <rsurendar26@gmail.com>
2022-11-24 14:10:36 -0600

    es6 examples

---
+++ es6/ex_array.js
@@ -0,0 +1,98 @@
+let arr = [10,20,30,40]
+arr.pop();
+console.log("array -> pop method", arr);
+
+let x = 10;
+let x = 15;
+
+
+v
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
++
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+r x = "John Doe";
+
+var x = 0;
+
+console.log("x", x);
+
+const address = {
+    housenumber: "1141",
+    street:"orleans Drive",
+    citi:"mundelein",
+    state:"illiois",
+    zip:"60060"
+};
+
+address.housenumber = "1135";
+
+console.log("address", address);
